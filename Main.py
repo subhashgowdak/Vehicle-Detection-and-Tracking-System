acc43682ab98a52371d58912b4cc3e32bd460813
Subhash <subhashgowdak03@gmail.com>
2025-11-22 11:40:14 +0530

    draw circle at the center of each detected object

--- Main.py
+++ Main.py
@@ -29,10 +29,14 @@ while cap.isOpened():
         #loop through each detected object
         for box, track_id, class_idx, conf in zip(boxes, track_ids, class_indices, confidences):
             x1,y1,x2,y2=map(int,box)
+            cx= (x1+x2) // 2 #calculate the center point
+            cy= (y1+y2) // 2
+
             class_name=class_list[class_idx]
             cv2.putText(frame,f"ID: {track_id} {class_name}",(x1,y1-10),
                         cv2.FONT_HERSHEY_SIMPLEX,0.6,(0,255,255),2)
             cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.circle(frame,(cx,cy),4,(0,0,255),-1) #draw circle at the center of each detected object
 
     #show the frame
     cv2.imshow("YOLO object tracking and counting", frame)
